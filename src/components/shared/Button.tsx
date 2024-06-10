3f10163e7c461d760bc20852a4e3135bdd9d8148
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-10 23:16:08 +0900

    feat: 버튼 컴포넌트 구현

---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,61 @@
+import { ButtonHTMLAttributes } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/utils';
+
+export const ButtonVariants = cva(
+  `
+  flex justify-center items-center w-full h-10 rounded-button
+  `,
+  {
+    variants: {
+      variant: {
+        solid: 'bg-blue text-white',
+        outline: 'border-blue text-black',
+        disabled: 'bg-gray200 text-black',
+      },
+      size: {
+        sm: 'h-8 font-normal',
+        md: 'h-10 font-medium',
+      },
+    },
+    defaultVariants: {
+      variant: 'solid',
+      size: 'md',
+    },
+  }
+);
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof ButtonVariants> {
+  children?: React.ReactNode;
+  disabled?: boolean;
+  isShadow?: boolean;
+  onClick: (value?: any) => void;
+}
+
+export const Button = ({
+  children,
+  variant,
+  size,
+  disabled,
+  isShadow,
+  className,
+  onClick,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={cn(
+        isShadow && 'shadow-button',
+        ButtonVariants({ variant: disabled ? 'disabled' : variant, size }),
+        className
+      )}
+      onClick={disabled ? () => {} : onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
