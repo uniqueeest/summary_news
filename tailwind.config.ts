a4fe4beaea2f0f5de4ed2797420e081f671b6e7f
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-10 23:12:13 +0900

    style: 스타일 토큰 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        black: '#0D141C',
+        blue: '#1A80E5',
+        deep_blue: '#4F7396',
+        gray100: '#D1DBE8',
+        gray200: '#E8EDF2',
       },
     },
   },
