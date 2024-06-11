45b717ea29eb3a0ae8571e82729969055064be98
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-11 23:25:47 +0900

    fix: 헤더 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,31 @@
-export const Header = () => {
-  return <div>Header</div>;
+import { HTMLAttributes, ReactNode } from 'react';
+
+import { cn } from '@/utils';
+
+interface HeaderProps extends HTMLAttributes<HTMLDivElement> {
+  leftSlot?: ReactNode;
+  title: string;
+  rightSlot?: ReactNode;
+}
+
+export const Header = ({
+  leftSlot,
+  title,
+  rightSlot,
+  className,
+  ...props
+}: HeaderProps) => {
+  return (
+    <div
+      className={cn(
+        'relative flex justify-center items-center p-4 w-full font-bold text-18',
+        className
+      )}
+      {...props}
+    >
+      {leftSlot && <div className="absolute top-4 left-4">{leftSlot}</div>}
+      {title}
+      {rightSlot && <div className="absolute top-4 right-4">{rightSlot}</div>}
+    </div>
+  );
 };
