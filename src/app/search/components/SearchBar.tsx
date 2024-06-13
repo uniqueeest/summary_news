2eb04f094b381f6282cfd55595f49914e1696f50
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-13 20:56:08 +0900

    feat: 서치 바 컴포넌트 구현

---
+++ src/app/search/components/SearchBar.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import {
+  forwardRef,
+  Ref,
+  KeyboardEvent,
+  useRef,
+  ChangeEvent,
+  HTMLAttributes,
+  useImperativeHandle,
+} from 'react';
+import Image from 'next/image';
+import { cva } from 'class-variance-authority';
+
+import { cn } from '@/utils';
+
+type SearchBarProps = {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSearch: () => void;
+  containerClassName?: string;
+} & Omit<HTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>;
+
+const SearchBar = forwardRef(
+  (
+    {
+      value,
+      onChange,
+      onSearch,
+      containerClassName,
+      className,
+      ...props
+    }: SearchBarProps,
+    ref: Ref<HTMLInputElement>
+  ) => {
+    const internalRef = useRef<HTMLInputElement>(null);
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    useImperativeHandle(ref, () => internalRef.current as HTMLInputElement);
+
+    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        e.preventDefault();
+        buttonRef.current?.click();
+      }
+    };
+
+    return (
+      <div
+        role="search"
+        className={cn(
+          'flex gap-3 items-center bg-gray200 px-4 py-3 w-full shadow-out rounded-button',
+          containerClassName
+        )}
+      >
+        <Image src="/icons/search.svg" alt="search" width={20} height={20} />
+        <input
+          ref={internalRef}
+          value={value}
+          onChange={onChange}
+          placeholder="Stocks, Crypto..."
+          aria-label="Stocks, Crypto..."
+          onKeyDown={handleKeyDown}
+          className={cn(
+            'flex-1 bg-transparent text-deep_blue outline-none',
+            className
+          )}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
+
+SearchBar.displayName = 'SearchBar';
+
+export { SearchBar };
