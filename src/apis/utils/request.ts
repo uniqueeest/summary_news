82834c2129d40670443b1be693261a8f9a77ae4d
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-15 15:19:23 +0900

    feat: request 유틸 추가

---
+++ src/apis/utils/request.ts
@@ -0,0 +1,64 @@
+type Options = Omit<RequestInit, 'body'>;
+
+type CreateOptions = {
+  baseURL: string;
+} & Options;
+
+class Request {
+  private baseURL: string;
+  private options: Options;
+
+  constructor(options?: CreateOptions) {
+    const { baseURL, ...otherOptions } = options || {};
+    this.baseURL = baseURL || '';
+    this.options = otherOptions;
+  }
+
+  create(options?: CreateOptions) {
+    return new Request(options);
+  }
+
+  get(url: string, options?: Options) {
+    const fullUrl = this.constructURL(url);
+    return this.fetch(fullUrl, {
+      method: 'GET',
+      ...this.options,
+      ...options,
+    });
+  }
+
+  post(url: string, data: unknown, options?: Options) {
+    const fullUrl = this.constructURL(url);
+    return this.fetch(fullUrl, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      ...this.options,
+      ...options,
+    });
+  }
+
+  private constructURL(url: string) {
+    if (this.baseURL === '') {
+      return url;
+    }
+
+    return new URL(url, this.baseURL).href;
+  }
+
+  private async fetch(url: string, options: RequestInit) {
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return await response.json();
+    } catch (error: unknown) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
+
+const request = new Request();
+
+export { request };
