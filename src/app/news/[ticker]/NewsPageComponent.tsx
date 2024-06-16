ec2ccf93efd1b531a96f5aa97209f074644dc146
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-16 23:04:03 +0900

    fix: 뉴스 데이터 연동

--- src/app/news/[ticker]/NewsPageComponent.tsx
+++ src/app/news/[ticker]/NewsPageComponent.tsx
@@ -1,20 +1,40 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { fetchNews } from './utils';
+import { NewsType } from '@/models';
 
 export const NewsPageComponent = ({ ticker }: { ticker: string }) => {
+  const [newsList, setNewsList] = useState<NewsType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
+
       try {
         const response = await fetchNews(ticker);
-        console.log(response);
+        setNewsList(response.slice(0, 12));
       } catch (err) {
         console.log(err);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchData();
   }, [ticker]);
 
-  return <div></div>;
+  if (isLoading) {
+    <div>loading...</div>;
+  }
+
+  return (
+    <section className="flex flex-col gap-3">
+      {newsList.map((news) => (
+        <div key={news.id}>
+          <h2>{news.headline}</h2>
+        </div>
+      ))}
+    </section>
+  );
 };
