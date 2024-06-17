e8b22f19da238a62cf8e8b7b002bc0f8771ccb8f
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-18 00:21:08 +0900

    fix: base api 추가

--- src/apis/baseApi.ts
+++ src/apis/baseApi.ts
@@ -1,11 +1,21 @@
 import { request } from './utils';
 
 const QUANTUS_URL = process.env.NEXT_PUBLIC_QUANTUS_URL;
+const FINNHUB_URL = process.env.NEXT_PUBLIC_FINNHUB_URL;
+const DEEPL_URL = process.env.NEXT_PUBLIC_DEEPL_URL;
 
-if (!QUANTUS_URL) {
+if (!QUANTUS_URL || !FINNHUB_URL || !DEEPL_URL) {
   throw new Error('MISS URL');
 }
 
 export const baseApi = request.create({
   baseURL: QUANTUS_URL,
+});
+
+export const finnhubApi = request.create({
+  baseURL: FINNHUB_URL,
+});
+
+export const deeplApi = request.create({
+  baseURL: DEEPL_URL,
 });
