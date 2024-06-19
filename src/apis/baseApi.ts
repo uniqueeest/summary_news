1302f6acadc0d5e4825b9cbb41998d4f55bc9ff2
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-19 22:44:26 +0900

    remove: 미사용 api 제거

--- src/apis/baseApi.ts
+++ src/apis/baseApi.ts
@@ -2,9 +2,8 @@ import { request } from './utils';
 
 const QUANTUS_URL = process.env.NEXT_PUBLIC_QUANTUS_URL;
 const FINNHUB_URL = process.env.NEXT_PUBLIC_FINNHUB_URL;
-const DEEPL_URL = process.env.NEXT_PUBLIC_DEEPL_URL;
 
-if (!QUANTUS_URL || !FINNHUB_URL || !DEEPL_URL) {
+if (!QUANTUS_URL || !FINNHUB_URL) {
   throw new Error('MISS URL');
 }
 
@@ -14,8 +13,4 @@ export const baseApi = request.create({
 
 export const finnhubApi = request.create({
   baseURL: FINNHUB_URL,
-});
-
-export const deeplApi = request.create({
-  baseURL: DEEPL_URL,
 });
