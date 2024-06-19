a757e2aa2f4eb5b2161d210de6cab6c8767b5963
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-19 22:45:06 +0900

    feat: 뉴스 리스트 컴포넌트 구현

--- src/app/news/[ticker]/NewsPageComponent.tsx
+++ src/app/news/[ticker]/NewsPageComponent.tsx
@@ -1,40 +1,64 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { fetchNews } from './utils';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { fromUnixTime, format } from 'date-fns';
+
+import { Header, PageLayout } from '@/components/layout';
+import { Spacing } from '@/components/shared';
 import { NewsType } from '@/models';
 
-export const NewsPageComponent = ({ ticker }: { ticker: string }) => {
-  const [newsList, setNewsList] = useState<NewsType[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+type NewsList = {
+  translations: {
+    detected_source_language: string;
+    text: string;
+  }[];
+};
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await fetchNews(ticker);
-        setNewsList(response.slice(0, 12));
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-  }, [ticker]);
-
-  if (isLoading) {
-    <div>loading...</div>;
-  }
+export const NewsPageComponent = ({
+  ticker,
+  translateTitleList,
+  newsList,
+}: {
+  ticker: string;
+  translateTitleList: NewsList;
+  newsList: NewsType[];
+}) => {
+  const router = useRouter();
 
   return (
     <section className="flex flex-col gap-3">
-      {newsList.map((news) => (
-        <div key={news.id}>
-          <h2>{news.headline}</h2>
-        </div>
-      ))}
+      <Header
+        leftSlot={
+          <Image
+            src="/icons/go_back.svg"
+            className="pt-1"
+            width={18}
+            height={18}
+            alt="go-back"
+            onClick={() => router.back()}
+          />
+        }
+        title="news"
+      />
+      <PageLayout className="gap-3">
+        {translateTitleList.translations.map((news, index) => {
+          const { datetime, id } = newsList[index];
+          return (
+            <div
+              className="flex flex-col gap-1 border-b border-b-gray100"
+              key={news.text}
+              onClick={() => router.push(`/news/${ticker}/${id}`)}
+            >
+              <h2>{news.text}</h2>
+              <p className="mb-3 self-end text-12">
+                {format(fromUnixTime(datetime), 'yyyy-MM-dd HH:mm:ss')}
+              </p>
+            </div>
+          );
+        })}
+        <Spacing size={28} />
+      </PageLayout>
     </section>
   );
 };
